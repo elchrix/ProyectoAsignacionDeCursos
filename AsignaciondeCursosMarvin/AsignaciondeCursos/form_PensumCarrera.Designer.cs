a24294ca4d776ff2936f398199f36759541ccb10
Rouggeri <Marvin>
2016-07-30 21:32:57 -0600

    Creacion de PDF pensum solo falta colocar dinamico los semestres

--- AsignaciondeCursosMarvin/AsignaciondeCursos/form_PensumCarrera.Designer.cs
+++ AsignaciondeCursosMarvin/AsignaciondeCursos/form_PensumCarrera.Designer.cs
@@ -43,6 +43,7 @@
             this.btn_GenerarPensumPdf = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgv_pensum)).BeginInit();
             this.SuspendLayout();
             // 
@@ -146,6 +147,8 @@
             // 
             // dgv_pensum
             // 
+            this.dgv_pensum.AllowUserToAddRows = false;
+            this.dgv_pensum.AllowUserToDeleteRows = false;
             this.dgv_pensum.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dgv_pensum.Location = new System.Drawing.Point(30, 248);
             this.dgv_pensum.Name = "dgv_pensum";
@@ -180,11 +183,22 @@
             this.textBox1.Size = new System.Drawing.Size(100, 98);
             this.textBox1.TabIndex = 15;
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(81, 94);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 16;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // form_PensumCarrera
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(596, 432);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.btn_GenerarPensumPdf);
@@ -222,9 +236,10 @@
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.ComboBox cbo_facultad;
         private System.Windows.Forms.Button btn_conexion;
-        private System.Windows.Forms.DataGridView dgv_pensum;
         private System.Windows.Forms.Button btn_GenerarPensumPdf;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        public System.Windows.Forms.DataGridView dgv_pensum;
     }
 }
--- AsignaciondeCursosMarvin/AsignaciondeCursos/form_PensumCarrera.cs
+++ AsignaciondeCursosMarvin/AsignaciondeCursos/form_PensumCarrera.cs
@@ -11,6 +11,7 @@ using System.IO;
 using MySql.Data.MySqlClient;
 using iTextSharp.text;
 using iTextSharp.text.pdf;
+using System.Diagnostics;
 
 namespace AsignaciondeCursos
 {
@@ -95,6 +96,7 @@ namespace AsignaciondeCursos
         {
             cbo_pensum.DisplayMember = "anio_pensum";
             cbo_pensum.ValueMember = "anio_pensum";
+            
 
             anio_pensum = cbo_pensum.SelectedValue.ToString().Trim();
             id_carrera = cbo_carrera.SelectedValue.ToString().Trim();
@@ -115,46 +117,125 @@ namespace AsignaciondeCursos
         {
             
             int contador = 0;
-            
+            int recorre = 0;
+            String facultad ;
+            dgv_pensum.Sort(dgv_pensum.Columns["codigo_curso"], ListSortDirection.Ascending);
 
             try
             {
-                String ruta = @"C:\Users\Marvin\Documents\PensumsGenerados";
+
+                String ruta = "";
+                ruta = @"C:\Users\Marvin\Documents\PensumsGenerados";
 
                 Document DocumentoPensum = new Document(iTextSharp.text.PageSize.LETTER,10,10,42,35);
                 PdfWriter nombre_doc = PdfWriter.GetInstance(DocumentoPensum, new FileStream(ruta + "pensum.pdf", FileMode.Create));
                 DocumentoPensum.Open();
+                // ---------------------- la hoja de de PDF tiene un ancho de 600 
+                //------------------------ LOGO UMG 
+                iTextSharp.text.Image logo = iTextSharp.text.Image.GetInstance("logo2.jpg");
+                logo.SetAbsolutePosition(30f,650f);
+                //logo.Alignment = Element.ALIGN_LEFT;
+                logo.ScaleAbsoluteHeight(125);
+                logo.ScaleAbsoluteWidth(125);
 
-           
-            
-            do
+                DocumentoPensum.Add(logo);
+
+                // Encabezado umg
+                Paragraph encabezado = new Paragraph(new Phrase("\nUniversidad Complutense de Madrid\n" + cbo_facultad.Text + "\n" + cbo_carrera.Text + "\nPensum "+cbo_pensum.Text, new iTextSharp.text.Font(iTextSharp.text.Font.NORMAL, 13f, iTextSharp.text.Font.ITALIC, iTextSharp.text.BaseColor.BLACK)));
+                encabezado.Alignment = Element.ALIGN_CENTER;
+                DocumentoPensum.Add(encabezado);
+
+                Paragraph titulos = new Paragraph(new Phrase("\n\n\n\n       Código       Curso           Créditos        Requisitos", new iTextSharp.text.Font(iTextSharp.text.Font.NORMAL, 13f, iTextSharp.text.Font.BOLD, iTextSharp.text.BaseColor.BLACK)));
+                titulos.Alignment = Element.ALIGN_CENTER;
+                DocumentoPensum.Add(titulos);
+                //String ciclo_uno = dgv_pensum.Rows[contador].Cells[3].Value.ToString();
+
+                do
             {
+                   // String ciclo_uno = dgv_pensum.Rows[contador].Cells[3].Value.ToString();
+
                     switch (contador)
                     {
                         case 0:
-                            Paragraph primersem = new Paragraph(new Phrase("Primer Semestre\n\n", new iTextSharp.text.Font(iTextSharp.text.Font.NORMAL, 11f, iTextSharp.text.Font.ITALIC, iTextSharp.text.BaseColor.BLACK)));
-                            primersem.Alignment = Element.ALIGN_LEFT;
-                            DocumentoPensum.Add(primersem);break;
-                            
+                                                       
+                                Paragraph primersem = new Paragraph(new Phrase("\nPrimer Semestre", new iTextSharp.text.Font(iTextSharp.text.Font.NORMAL, 12f, iTextSharp.text.Font.BOLD, iTextSharp.text.BaseColor.BLACK)));
+                                primersem.Alignment = Element.ALIGN_LEFT;
+                                DocumentoPensum.Add(primersem); break;
+                                                                                    
                         case 3:
-                            Paragraph segundosem = new Paragraph(new Phrase("Segundo Semestre\n\n", new iTextSharp.text.Font(iTextSharp.text.Font.NORMAL, 11f, iTextSharp.text.Font.ITALIC, iTextSharp.text.BaseColor.BLACK)));
+                            Paragraph segundosem = new Paragraph(new Phrase("\nSegundo Semestre", new iTextSharp.text.Font(iTextSharp.text.Font.NORMAL, 12f, iTextSharp.text.Font.BOLD, iTextSharp.text.BaseColor.BLACK)));
                             segundosem.Alignment = Element.ALIGN_LEFT;
                             DocumentoPensum.Add(segundosem);break;
-                            
+
+                        case 6:
+                            Paragraph tercersem = new Paragraph(new Phrase("\nTercer Semestre", new iTextSharp.text.Font(iTextSharp.text.Font.NORMAL, 12f, iTextSharp.text.Font.BOLD, iTextSharp.text.BaseColor.BLACK)));
+                            tercersem.Alignment = Element.ALIGN_LEFT;
+                            DocumentoPensum.Add(tercersem); break;
+
+                        case 9:
+                            Paragraph cuartosem = new Paragraph(new Phrase("\nCuarto Semestre", new iTextSharp.text.Font(iTextSharp.text.Font.NORMAL, 12f, iTextSharp.text.Font.BOLD, iTextSharp.text.BaseColor.BLACK)));
+                            cuartosem.Alignment = Element.ALIGN_LEFT;
+                            DocumentoPensum.Add(cuartosem); break;
+
+                        case 12:
+                            Paragraph quinsem = new Paragraph(new Phrase("\nQuinto Semestre", new iTextSharp.text.Font(iTextSharp.text.Font.NORMAL, 12f, iTextSharp.text.Font.BOLD, iTextSharp.text.BaseColor.BLACK)));
+                            quinsem.Alignment = Element.ALIGN_LEFT;
+                            DocumentoPensum.Add(quinsem); break;
+
+                        case 15:
+                            Paragraph sexsem = new Paragraph(new Phrase("\nSexto Semestre", new iTextSharp.text.Font(iTextSharp.text.Font.NORMAL, 12f, iTextSharp.text.Font.BOLD, iTextSharp.text.BaseColor.BLACK)));
+                            sexsem.Alignment = Element.ALIGN_LEFT;
+                            DocumentoPensum.Add(sexsem); break;
+
+                        case 18:
+                            Paragraph septsem = new Paragraph(new Phrase("\nSeptimo Semestre", new iTextSharp.text.Font(iTextSharp.text.Font.NORMAL, 12f, iTextSharp.text.Font.BOLD, iTextSharp.text.BaseColor.BLACK)));
+                            septsem.Alignment = Element.ALIGN_LEFT;
+                            DocumentoPensum.Add(septsem); break;
+
+                        case 21:
+                            Paragraph octsem = new Paragraph(new Phrase("\nOctavo Semestre", new iTextSharp.text.Font(iTextSharp.text.Font.NORMAL, 12f, iTextSharp.text.Font.BOLD, iTextSharp.text.BaseColor.BLACK)));
+                            octsem.Alignment = Element.ALIGN_LEFT;
+                            DocumentoPensum.Add(octsem); break;
+
+                        case 24:
+                            Paragraph novsem = new Paragraph(new Phrase("\nNoveno Semestre", new iTextSharp.text.Font(iTextSharp.text.Font.NORMAL, 12f, iTextSharp.text.Font.BOLD, iTextSharp.text.BaseColor.BLACK)));
+                            novsem.Alignment = Element.ALIGN_LEFT;
+                            DocumentoPensum.Add(novsem); break;
+
+                        case 27:
+                            Paragraph decsem = new Paragraph(new Phrase("\nDecimo Semestre", new iTextSharp.text.Font(iTextSharp.text.Font.NORMAL, 12f, iTextSharp.text.Font.BOLD, iTextSharp.text.BaseColor.BLACK)));
+                            decsem.Alignment = Element.ALIGN_LEFT;
+                            DocumentoPensum.Add(decsem); break;
                     }
-                Paragraph titulo_decripcion_auto = new Paragraph(new Phrase("\n" + dgv_pensum.Rows[contador].Cells[1].Value.ToString() + dgv_pensum.Rows[contador].Cells[2].Value.ToString(), new iTextSharp.text.Font(iTextSharp.text.Font.NORMAL, 11f, iTextSharp.text.Font.ITALIC, iTextSharp.text.BaseColor.BLACK)));
-                titulo_decripcion_auto.Alignment = Element.ALIGN_CENTER;
+
+                    // Parrafo con los campos de los cursos de la carrera seleccioada
+
+                Paragraph titulo_decripcion_auto = new Paragraph(new Phrase("\n                   "+dgv_pensum.Rows[contador].Cells[2].Value.ToString().Trim()+"         "+dgv_pensum.Rows[contador].Cells[1].Value.ToString().Trim()+"                "+dgv_pensum.Rows[contador].Cells[4].Value.ToString().Trim()+"            "+dgv_pensum.Rows[contador].Cells[5].Value.ToString() , new iTextSharp.text.Font(iTextSharp.text.Font.NORMAL, 11f, iTextSharp.text.Font.ITALIC, iTextSharp.text.BaseColor.BLACK)));
+                titulo_decripcion_auto.Alignment = Element.ALIGN_LEFT;
                 DocumentoPensum.Add(titulo_decripcion_auto);
                 contador++;
             }
-            while (contador <= 29);
+            while (contador < dgv_pensum.Rows.Count);
                     
-                DocumentoPensum.Close();
-                System.Diagnostics.Process.Start(ruta + "pensum.pdf");
+                DocumentoPensum.Close(); // se cierra el documento una ves realizados los cambios
+
+                /*foreach (Process proceso in Process.GetProcesses())
+                {
+                    if (proceso.ProcessName == ruta + "pensum.pdf") ;
+                    {
+                        proceso.Kill();
+                    }
+
+                }*/
+
+                System.Diagnostics.Process.Start(ruta + "pensum.pdf"); // el documento PDF se ejecuta automaticamente para que no se haga el doble cli
             }
             catch (Exception ex)
             {
                 MessageBox.Show(ex.Message);
+               
+               
+
             }
         }
 
@@ -162,20 +243,36 @@ namespace AsignaciondeCursos
 
         private void button1_Click(object sender, EventArgs e)
         {
-            textBox1.Text =  dgv_pensum.Rows[0].Cells[1].Value.ToString();
-            for (int i=1;i<=5;i++)
-            {
-                textBox1.Text = textBox1.Text+ dgv_pensum.Rows[i].Cells[1].Value.ToString();
-            }
-            /*int contador2=0;
-            while (contador2 <= 30)
-            {
-                //string nombrecu = dgv_pensum.Rows[contador2].Cells[0].Value.ToString();
+            /*int selectedIndex = cbo_facultad.SelectedIndex;
+            int selectedItem = cbo_facultad.SelectedIndexChanged;
 
-                textBox1.Text = dgv_pensum.Rows[contador2].Cells[0].Value.ToString(); 
+            MessageBox.Show("Item seleccionado: " +selectedItem.ToString()+"\n"+"Index: "+selectedIndex.ToString());*/
+            //textBox1.Text = cbo_facultad.Text;
 
-                //contador2++;
-            }*/
+            
+            
+                Int32 i;
+                String celda;
+                DataGridViewCell dgc;
+                //Recorremos el DataGridView con un bucle for 
+                for (i = 0; i < dgv_pensum.Rows.Count; i++)
+                {
+                    /*dgc = dgv_pensum.Rows[i].Cells[0];
+                    celda = ((String)dgc.Value) + "\r\n";
+                    textBox1.Text += celda.Replace(".", ",");*/
+                }
+                MessageBox.Show("numero de columnas" + i);
+            
+
+            
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //dgv_pensum.Sort(dgv_pensum.Rows(1), ListSortDirection.Ascending);
+            dgv_pensum.Sort(dgv_pensum.Columns["codigo_curso"], ListSortDirection.Ascending);
+
         }
     }
 }
